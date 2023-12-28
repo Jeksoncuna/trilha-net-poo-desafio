67dce54d2acae703d0eeaad35ee937f02d152501
JeksonCuna <jeksoncosta1@gmail.com>
2023-12-28 20:50:39 -0300

    Trilha concluida com sucesso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia : ");
+Smartphone nokia =  new Nokia(numero: "1234", modelo: "Modelo 1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "2314", modelo: "Modelo 12", imei: "1132111", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
